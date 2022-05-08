caf0e35fd8a8846f30b1d44197e16d1aa59cdf93
jeroso <jeroso@nate.com>
2022-05-08 17:07:05 +0900

    service 테스트 생성 및 DI 적용

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -2,14 +2,19 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    // new 로 생성하지않고 외부에서 값을 넣어주는 방법을 DI(Dependency Injection)이라고 한다.
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
 
     /**
      * 회원가입
